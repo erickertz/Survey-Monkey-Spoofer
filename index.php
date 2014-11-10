cdab19e0dd51603f5c790f0ec18374c202a6989a
erickertz <erickertz@gmail.com>
2014-11-10 16:30:24 -0500

    added setSurveyUrl

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require("lib/SurveyMonkeySpoof/SurveyMonkeySpoof.php");
 use lib\SurveyMonkeySpoof\SurveyMonkeySpoof;
 
 $surveyMonkeySpoof = new SurveyMonkeySpoof();
+$surveyMonkeySpoof->setSurveyUrl("https://www.surveymonkey.com/r/");
 $surveyMonkeySpoof->setSurveyId("2S833S2");
 $surveyMonkeySpoof->setSurveyQuestionId("725782097");
 $surveyMonkeySpoof->setSurveyAnswerId("8282574965");
